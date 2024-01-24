edcf42c94cf4c2f91749c75bbb1b2f02c4324637
id10tothe9 <id10tothe9@googlemail.com>
2024-01-24 22:40:16 +0000

    add exercises

--- rest-parameter-syntax.js
+++ rest-parameter-syntax.js
@@ -4,7 +4,21 @@
  */
 
 // Regular function call 
+const sumAll = (a,b,c) => a+b+c;
+let sum1 = sumAll(1,2,3);
+//console.log(sum1);
 
 // Extra arguments are ignored
+let sum2 = sumAll(1,2,3,4,5,6);
+//console.log(sum2);
 
 // Function using ...rest
+const sumRest = (a,b,c, ...rest) => {
+    let sum = a+b+c;
+    for (i of rest) {
+        sum += i;
+    }
+    return sum;
+}
+let sum3 = sumRest(1,2,3,4,5,6);
+console.log(sum3);
